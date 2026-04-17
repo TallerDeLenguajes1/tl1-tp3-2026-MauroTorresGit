7314754f7d65985d4413dab438348242b5687421
MauroTorresGit <maurotorres2802@gmail.com>
2026-04-17 14:14:54 -0300

    buscar nombre id

--- ArregloDeNombres.c
+++ ArregloDeNombres.c
@@ -3,37 +3,56 @@
 #include <string.h>
 
 void mostrarPersonas (char *nom[], int cant) {
-    //printf("%s", nom);
-    //puts(nom);
+    for (int i = 0; i < cant; i++) printf("%d- %s\n", i+1, nom[i]);
+}
+
+void buscarNombre (int id, int cant, char *nom[]) {
+    int aux = 0;
     for (int i = 0; i < cant; i++) {
-        printf("%d- ", i+1);
-        printf("%s\n", nom[i]);
+        if (id-1 == i) {
+            printf("Buscado por id: %d- %s\n", i+1, nom[i]);
+            aux = 1;
+            break;
+        } else {
+            if (i+1 == cant && aux == 0) {
+                printf("No se encontro el valor buscado");
+            }
+        }
     }
 }
 
-int main () {
-
-    
-
-    char *nombres[5];
-    char *buff;
-    int cant = 3;
-
-    for (int i = 0; i < cant; i++) {
-        nombres[i] = (char *) malloc(100 * sizeof(char));
+void buscarNombre2 (int id, int cant, char *nom[]) {
+    int i = 0;
+    for (i = 0; i < cant; i++) {
+        if (id-1 == i) {
+            printf("Buscado por id: %d- %s\n", i+1, nom[i]);
+            break;
+        } 
     }
-    //buff = (char *) malloc(100 * sizeof(char));
+    if (i == cant) printf("No se encontro el valor buscado");
+}
 
+int main () {
+    char *nombres[5];
+    int cant = 5, id = 5;
+
+    for (int i = 0; i < cant; i++) 
+        nombres[i] = (char *) malloc(100 * sizeof(char));
+    
     for (int i = 0; i < cant; i++) {
         printf("Ingrese nombres:\n");
         gets(nombres[i]);
     }
 
-    
     mostrarPersonas(nombres, cant);
 
-    //free(nombres);
-    //free(buff);
+    printf("Ingrese un id:\n");
+    scanf("%d", &id);
+
+    buscarNombre2(id, cant, nombres);
+
+    for (int i = 0; i < cant; i++)
+        free(nombres[i]);
 
     return 0;
 }
